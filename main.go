f8f51e59037c50818dc9524b7141a2a2bc157233
savin000 <58307835+savin000@users.noreply.github.com>
2025-07-07 22:16:08 +0300

    add tcp listener, exit on ctrl+c,

--- main.go
+++ main.go
@@ -4,35 +4,56 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net"
 	"os"
+	"os/signal"
 	"strings"
 )
 
 func main() {
-	file, err := os.Open("message.txt")
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		os.Exit(0)
+	}()
+
+	listener, err := net.Listen("tcp", ":42069")
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer func() { _ = listener.Close() }()
 
-	linesChannel := getLinesChannel(file)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	for line := range linesChannel {
-		fmt.Printf("read: %s\n", line)
+		fmt.Println("Connection Accepted")
+
+		linesChannel := getLinesChannel(conn)
+
+		for line := range linesChannel {
+			fmt.Printf("%s\n", line)
+		}
+
+		fmt.Println("Connection Closed")
 	}
 }
 
-func getLinesChannel(f io.ReadCloser) <-chan string {
+func getLinesChannel(conn net.Conn) <-chan string {
 	result := make(chan string)
 	chunkSize := 8
 	chunk := make([]byte, chunkSize)
 	currentLine := ""
 
 	go func() {
-		defer func() { _ = f.Close() }()
+		defer func() { _ = conn.Close() }()
 		defer close(result)
 
 		for {
-			chunkBytes, err := f.Read(chunk)
+			chunkBytes, err := conn.Read(chunk)
 
 			if err != nil {
 				if err == io.EOF {
